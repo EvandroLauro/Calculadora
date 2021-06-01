24e1226522e243d84e62d1c3088922abb078f2f2
Evandro Lauro <evandrolauro1992@gmail.com>
2021-06-01 13:38:03 -0300

    Melhorando o codigo

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 
-
 function atualizaVisor(numero) {
-    document.getElementById("visor").innerHTML = numero;
+    document.getElementById("visor").innerHTML = numero
 }
 
 function enviaNumero(numero) {
     let numeroVisor = document.getElementById("visor").innerHTML
     if (numeroVisor.length < 8) {
         if (numero == 0 && numero == numeroVisor) {
-            numeroVisor = 0
+            atualizaVisor(0)
         } else {
             if (numeroVisor == 0) {
-                numeroVisor = numero
+                atualizaVisor(numero)
             } else {
-                numeroVisor = numeroVisor + numero 
+                atualizaVisor(numeroVisor + numero) 
             }
         }
-        atualizaVisor(numeroVisor)
     }
 }
 
@@ -25,7 +23,7 @@ function limpa() {
     if (numeroVisor.length == 1) {
         atualizaVisor(0)
     } else {
-        let numeroLength =  numeroVisor.length
+        let numeroLength = numeroVisor.length
         let tamanhanhoDoCorte = numeroLength - 1
         let fatiado = numeroVisor.slice(0, tamanhanhoDoCorte)
         atualizaVisor(fatiado)
