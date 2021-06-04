9735023aada323b90f5bb5ada683f5556271d7c6
Evandro Lauro <evandrolauro1992@gmail.com>
2021-06-04 09:49:57 -0300

    ponto de restauração

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const execucao = {
+    andamento : false,
+    n1 : "",
+    n2 : ""
+}
+
 const visor = {
     atualiza : function(numero) {
         document.getElementById("visor").innerHTML = numero
@@ -9,16 +15,27 @@ const visor = {
 
 const envia = {
     numero : function(numero) {
-        let numeroVisor = visor.exibido()
-        let numeroVisorSemVirgula = numeroVisor.replace(",", "")
-        if (numeroVisorSemVirgula.length < 8) {
-            if (numero == 0 && numero == numeroVisor) {
-                visor.atualiza(0)
-            } else {
-                if (numeroVisor == 0) {
-                    visor.atualiza(numero)
+        if (execucao.andamento == true) {
+                visor.atualiza(numero)
+                execucao.n2 = numero
+                execucao.andamento = false
+        } else {
+            let numeroVisor = visor.exibido()
+            let numeroVisorSemVirgula = numeroVisor.replace(",", "")
+            if (numeroVisorSemVirgula.length < 8) {
+                if (numero == 0 && numero == numeroVisor) {
+                    visor.atualiza(0)
                 } else {
-                    visor.atualiza(numeroVisor + numero) 
+                    if (execucao.n2 != "") {
+                        visor.atualiza(numeroVisor + numero)
+                        execucao.n2 = numeroVisor + numero
+                    } else if (numeroVisor == 0) {
+                        visor.atualiza(numero)
+                        execucao.n1 = numero
+                    } else {
+                        visor.atualiza(numeroVisor + numero)
+                        execucao.n1 = numeroVisor + numero
+                    }
                 }
             }
         }
@@ -32,10 +49,13 @@ const envia = {
 };
 
 const limpar = {
-    ce : function() {
-        visor.atualiza(0)
-    },
     c : function() {
+        visor.atualiza(0)
+        execucao.andamento = false
+        execucao.n1 = ""
+        execucao.n2 = ""
+    },
+    ce : function() {
         let numeroVisor = visor.exibido()
         if (numeroVisor.length == 1) {
             visor.atualiza(0)
@@ -46,5 +66,31 @@ const limpar = {
             visor.atualiza(fatiado)
         }
     }
+};
+
+function operacao(operador) {
+    if (execucao.n2 == "") {
+        execucao. andamento = true
+        let numeroVisor = visor.exibido()
+        execucao.n1 = numeroVisor + operador
+    } 
+    
+    /*
+    if (numeroVisorSemVirgula != 0) {
+        if (execucao.n1 == "") {
+            execucao.n1 = numeroVisor + operador
+            visor.atualiza(0)
+            console.log(execucao.n1)
+        } else if (execucao.n2 == ""){
+            execucao.n2 = numeroVisor
+            let resultado = eval(execucao.n1 + execucao.n2)
+            execucao.n1 = resultado
+            execucao.n2 = ""
+            visor.atualiza(resultado)
+            console.log("visor", visor.exibido())
+            console.log("resultado", resultado)
+        }
+    }
+    */
 }
 
